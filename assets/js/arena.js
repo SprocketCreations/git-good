ec348678120916c59c8eaec8dc94aa14807b5e99
Kailen James <sevenflags57@gmail.com>
2023-01-11 13:47:55 -0800

    Added a lot more members

--- assets/js/arena.js
+++ assets/js/arena.js
@@ -1,3 +1,10 @@
+/**
+ * Enum for the current state of play.
+ * @Initializing The game is setting itself up.
+ * @Playing The game is allowing the players to play cards to the active battletrack.
+ * @Action The game is allowing the players to decide their card's actions.
+ * @Over The game has ended.
+ */
 const Stage = {
 	Initializing: 0,
 	Playing: 1,
@@ -5,6 +12,10 @@ const Stage = {
 	Over: 3,
 };
 
+/**
+ * This class is used to represent a player.
+ * I manages the mana, deck, and hand.
+ */
 class Player {
 	constructor() {
 		this.mana = 0;
@@ -33,37 +44,88 @@ class Card {
 	}
 }
 
+/**
+ * The battleline is one-half of the battletrack.
+ * It manages the hitpoints, and cards in play on
+ * its half.
+ */
 class Battleline {
-	constructor(hitpoints, healthNode, frontNode) {
+	/**
+	 * Constructs a new Battleline object.
+	 * 
+	 * @param {number} hitpoints The number of hitpoints this battleline should start with.
+	 * @param {Element} hitpointNode The element to write the number of hitpoints to.
+	 * @param {Element} zoneNode The element that contains the cards in play.
+	 */
+	constructor(hitpoints, hitpointNode, zoneNode) {
 		this.hitpoints = hitpoints;
-		this.healthNode = healthNode;
-		this.frontNode = frontNode;
+		this.healthNode = hitpointNode;
+		this.zoneNode = frontNode;
 		this.cards = [];
 	}
 }
 
-/* ___ Battletrack ___ */
-	// friendly cards : array
-	// enemy cards : array
+/**
+ * The Battletrack is the lane that play and action happens whithin.
+ * 
+ * The game consists of three battletracks, each of which has hitpoints,
+ * and cards in play.
+ */
 class Battletrack {
-	constructor(node, friendlyHealth = 40, enemyHealth = 40) {
-		this.friendlyBattleline = new Battleline(friendlyHealth);
-		this.enemyBattleline = new Battleline(enemyHealth);
+	/**
+	 * Constructs a new battletrack object.
+	 * 
+	 * @param {Element} node The root node on the DOM that represents this battletrack.
+	 * @param {Location} location The location data.
+	 * @param {number} friendlyHitpoints The amount of hitpoints the friendly side of the battletrack should start with. Defaults to 40.
+	 * @param {number} enemyHitpoints The amount of hitpoints the enemy side of the battletrack should start with. Defaults to 40.
+	 */
+	constructor(node, location, friendlyHitpoints = 40, enemyHitpoints = 40) {
+		// TODO: Crawl the node to get the hitpoints and selection area nodes
+		const friendlyHitpointsNode = null;
+		const friendlyCardZoneNode = null;
+		const enemyHitpointsNode = null;
+		const enemyCardZoneNode = null;
+
 		this.node = node;
-		this.location = {};
+		this.friendlyBattleline = new Battleline(friendlyHitpoints, friendlyHitpointsNode, friendlyCardZoneNode);
+		this.enemyBattleline = new Battleline(enemyHitpoints, enemyHitpointsNode, enemyCardZoneNode);
+		this.location = location;
+		// TODO: Crawl the node to get the elements used to indicate location
 		this.defeated = false;
 	}
-	getFriendlyHealth() {
-		return this.friendlyHealth;
+	/**
+	 * A getter for the hitpoints the friendly side of the battletrack.
+	 * @returns the amount of hitpoints the friendly side of the track has.
+	 */
+	getFriendlyHitpoints() {
+		return this.friendlyBattleline.getHitpoints();
 	}
-	getEnemyHealth() {
-		return this.enemyHealth;
+	/**
+	 * A getter for the hitpoints the enemy side of the battletrack.
+	 * @returns the amount of hitpoints the friendly side of the track has.
+	 */
+	getEnemyHitpoints() {
+		return this.enemyBattleline.getHitpoints();
 	}
-	setFriendlyHealth(health) {
-		this.friendlyHealth = health;
-		
-		this.node
+	/**
+	 * Sets the amount of hitpoints that the friendly side of the battletrack has.
+	 * @param {number} hitpoints The new amount of hitpoints.
+	 */
+	setFriendlyHealth(hitpoints) {
+		this.friendlyBattleline.setHitpoints(hitpoints);
 	}
+	/**
+	 * Sets the amount of hitpoints that the enemy side of the battletrack has.
+	 * @param {number} hitpoints The new amount of hitpoints.
+	 */
+	setEnemyHealth(hitpoints) {
+		this.enemyBattleline.setHitpoints(hitpoints);
+	}
+	/**
+	 * Gets the location data associated with this battlelane.
+	 * @returns the location.
+	 */
 	getLocation() {
 		return this.location;
 	}
