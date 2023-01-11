1345cdf3ffae0fca6a96d00a95fb975c47c389c0
Kailen James <sevenflags57@gmail.com>
2023-01-11 13:01:03 -0800

    Added class definitions

---
+++ assets/js/arena.js
@@ -0,0 +1,29 @@
+class Player {
+	constructor() {
+
+	}
+}
+
+class Card {
+	constructor() {
+		
+	}
+}
+
+class Battletrack {
+	constructor() {
+
+	}	
+}
+
+class Deck {
+	constructor() {
+
+	}
+}
+
+class Hand {
+	constructor() {
+		
+	}
+}
